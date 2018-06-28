6ecd67c5a84ab6ec0bb6b938ccbe12e41832c007
Gabriel Lindström <gabriel.lindstrom@helsinki.fi>
2018-06-28 15:55:38 +0300

    Fix imports

--- src/test/java/gabriel/betbot/service/TradeServiceTest.java
+++ src/test/java/gabriel/betbot/service/TradeServiceTest.java
@@ -3,14 +3,7 @@ package gabriel.betbot.service;
 import gabriel.betbot.trades.Odds;
 import gabriel.betbot.trades.OddsType;
 import java.math.BigDecimal;
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertTrue;
 
 /**
